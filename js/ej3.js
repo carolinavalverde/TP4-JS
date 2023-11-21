90193bc6c0121028cdca0a48eabb8acbea55bd52
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-21 17:57:47 -0300

    agrego ej3

--- js/ej3.js
+++ js/ej3.js
@@ -4,47 +4,15 @@ class Rectangulo {
     this.ancho = ancho;
   }
 
-const rectangulo1 = new Rectangulo(
-    2,
-    4,
-  );
-  console.log(rectangulo1);
-  document.write("<h1>Propiedades del Rectángulo 1:</h1><br>");
-
-  document.write(
-    "<h2>Perímetro del Rectángulo 1:</h2>" +
-      rectangulo.calcularPerimetro(rectangulo1) +
-      "<br>"
-  );
-  document.write(
-    "<h2>Área del Rectángulo:</h2> " + rectangulo.calcularArea() + "<br>"
-  );
-  
-
-  cambiarMedidas = () => {
-    const nuevoAlto = parseFloat(
-      prompt("Ingrese la altura del rectángulo:")
-    );
-    const nuevoAncho = parseFloat(
-      prompt("Ingrese el ancho del rectángulo:")
-    );
-
-    if (
-      !isNaN(nuevoAlto) &&
-      !isNaN(nuevoAncho) &&
-      nuevoAlto > 0 &&
-      nuevoAncho > 0
-    ) {
-      this.alto = nuevoAlto;
-      this.ancho = nuevoAncho;
-      console.log("Medidas modificadas");
-    } else {
-      console.log("Poner un nro válido");
-    }
+  modificarPropiedades = (nuevoAlto, nuevoAncho) => {
+    this.alto = nuevoAlto;
+    this.ancho = nuevoAncho;
   };
 
-  mostrarMedidas = () => {
-    console.log(`Rectángulo: Alto = ${this.alto}, Ancho = ${this.ancho}`);
+  mostrarPropiedades = () => {
+    document.write(
+      `<h1>Rectángulo:</h1> Alto = ${this.alto}, Ancho = ${this.ancho}<br>`
+    );
   };
 
   calcularPerimetro = () => 2 * (this.alto + this.ancho);
@@ -52,22 +20,40 @@ const rectangulo1 = new Rectangulo(
   calcularArea = () => this.alto * this.ancho;
 }
 
-const rectangulo = new Rectangulo(0, 0);
+const rectangulo1 = new Rectangulo(11, 6);
 
-rectangulo.cambiarMedidas();
-
-console.log("Propiedades del Rectángulo:");
-rectangulo.mostrarMedidas();
-console.log("Perímetro del Rectángulo:", rectangulo.calcularPerimetro());
-console.log("Área del Rectángulo:", rectangulo.calcularArea());
-
-document.write("<h1>Propiedades del Rectángulo:</h1><br>");
-rectangulo.mostrarMedidas();
+document.write("<h1>Propiedades del Rectángulo 1:</h1><br>");
+rectangulo1.mostrarPropiedades();
 document.write(
-  "<h2>Perímetro del Rectángulo:</h2> " +
-    rectangulo.calcularPerimetro() +
-    "<br>"
+  `<p>Perímetro del Rectángulo 1:</p> ${rectangulo1.calcularPerimetro()}<br>`
 );
 document.write(
-  "<h2>Área del Rectángulo:</h2> " + rectangulo.calcularArea() + "<br>"
+  `<p>Área del Rectángulo 1:</p> ${rectangulo1.calcularArea()}<br><br>`
+);
+
+const rectangulo2 = new Rectangulo(
+  parseFloat(prompt("Ingrese la altura del Rectángulo 2:")),
+  parseFloat(prompt("Ingrese el ancho del Rectángulo 2:"))
+);
+const rectangulo3 = new Rectangulo(
+  parseFloat(prompt("Ingrese la altura del Rectángulo 3:")),
+  parseFloat(prompt("Ingrese el ancho del Rectángulo 3:"))
+);
+
+document.write("<h1>Propiedades del Rectángulo 2:</h1><br>");
+rectangulo2.mostrarPropiedades();
+document.write(
+  `<p>Perímetro del Rectángulo 2:</p> ${rectangulo2.calcularPerimetro()}<br>`
+);
+document.write(
+  `<p>Área del Rectángulo 2:</p> ${rectangulo2.calcularArea()}<br><br>`
+);
+
+document.write("<h1>Propiedades del Rectángulo 3:</h1><br>");
+rectangulo3.mostrarPropiedades();
+document.write(
+  `<p>Perímetro del Rectángulo 3:</p> ${rectangulo3.calcularPerimetro()}<br>`
+);
+document.write(
+  `<p>Área del Rectángulo 3:</p> ${rectangulo3.calcularArea()}<br>`
 );
