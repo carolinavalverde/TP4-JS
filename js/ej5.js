7756b9fdd92cb493b3c33271a46a3b82f7762fc9
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-22 11:10:02 -0300

    agrego clase del ej5 / incompleto

---
+++ js/ej5.js
@@ -0,0 +1,11 @@
+class Persona {
+  constructor(nombre, edad, dni, sexo, peso, altura, añoDeNacimiento) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.sexo = sexo;
+    this.peso = peso;
+    this.altura = altura;
+    this.añoDeNacimiento = añoDeNacimiento;
+  }
+}
